1d35edbea174a5b31deaa607ed10b116d80bf3f9
Harshit <harshitchand37@gmail.com>
2025-02-01 13:24:57 +0530

    h

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -33,14 +33,14 @@ ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
-    'app.apps.AppConfig',
+    'app.apps.MyAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    
 ]
 
 MIDDLEWARE = [
